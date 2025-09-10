81978b045938f802dd678cdde4797164195d332c
gimigkk <88847585+Kue9@users.noreply.github.com>
2025-09-10 19:53:33 +0700

    Delete aboutme61 directory

--- aboutme61/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Halaman About Me berhasil dimuat!");
-
-    // Anda bisa menambahkan interaktivitas JavaScript di sini jika diperlukan.
-    // Contoh: animasi saat scroll, dll.
-});
