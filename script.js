4836330fd6e61191fd6190d4dcca18e0d4289d3b
gimigkk <88847585+Kue9@users.noreply.github.com>
2025-09-10 19:51:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Halaman About Me berhasil dimuat!");
+
+    // Anda bisa menambahkan interaktivitas JavaScript di sini jika diperlukan.
+    // Contoh: animasi saat scroll, dll.
+});
